5530923dabe0c647b7353948fd418cb08fba2454
chrisbaikas <38394038+chrisbaikas@users.noreply.github.com>
2025-07-08 08:57:27 +0300

    Update notification.c

--- terminal/src/notification.c
+++ terminal/src/notification.c
@@ -5,14 +5,14 @@ void *notificationThread(void *arg) {
 
     while (1) {
         if (sharedTransferNotification && *sharedTransferNotification) {
+            // Αν η μεταφορά προήλθε από άλλον χρήστη, εμφάνισε το όνομά του
             if (strcmp(u->name, sharedSenderUsername) != 0) {
-                printf("\n🔔 Someone transferred an account to you!\n");
+                printf("\n🔔 User '%s' transferred an account to you!\n", sharedSenderUsername);
                 fflush(stdout);
             }
-
+            // Μηδενίζουμε τη σημαία
             *sharedTransferNotification = 0;
         }
-
         usleep(500000); // 0.5s delay
     }
     return NULL;
